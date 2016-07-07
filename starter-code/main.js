23ebcb63ad1370e66bbc80fb3c72b029bc187aee
Yasuyoshi Sakamoto <Yasu@Yasuyoshis-iMac.local>
2016-07-06 21:38:14 -0700

    conditionals control flow

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,4 +1,21 @@
 var cardOne = "queen";
 var cardTwo = "queen";
-var cardTherr = "king";
+var cardThree = "king";
 var cardFour = "king";
+
+
+if(cardOne === cardTwo || cardThree === cardFour){
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+// if(cardOne === cardTwo || cardThree === cardFour){
+// 	alert("You found a match!");
+// } else if(cardOne === cardThree || cardOne === cardFour){
+// 	alert("Sorry, try again.");
+// } else if (cardTwo === cardThree || cardTwo === cardFour){
+// 	alert("Sorry, try again.");
+// } else {
+// 	alert("Plese pick your cards.");
+// }
