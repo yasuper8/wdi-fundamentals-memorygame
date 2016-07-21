22bcbed2878cec45056257745ba68aa884cc0a66
Yasuyoshi Sakamoto <Yasu@Yasuyoshis-iMac.local>
2016-07-21 00:51:45 -0700

    unit-10 new cards function

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,11 +4,11 @@ var cardThree = "king";
 var cardFour = "king";
 
 
-if(cardOne === cardTwo || cardThree === cardFour){
-	alert("You found a match!");
-} else {
-	alert("Sorry, try again.");
-}
+// if(cardOne === cardTwo || cardThree === cardFour){
+// 	alert("You found a match!");
+// } else {
+// 	alert("Sorry, try again.");
+// }
 
 // if(cardOne === cardTwo || cardThree === cardFour){
 // 	alert("You found a match!");
@@ -19,3 +19,22 @@ if(cardOne === cardTwo || cardThree === cardFour){
 // } else {
 // 	alert("Plese pick your cards.");
 // }
+
+
+var gameBoard = document.getElementById("game-board");
+
+// for(var i = 1; i <= 4; i++){
+// 	var newDivs = document.createElement("div");
+// 	newDivs.setAttribute("class", "card");
+// 	document.getElementById("game-board").appendChild(newDivs);
+// }
+
+function createdBoard(){
+	for(var i = 1; i <= 4; i++){
+		var newCards = document.createElement("divs");
+		newCards.setAttribute("class", "card");
+		document.getElementById("game-board").appendChild(newCards);
+	}
+}
+
+createdBoard();
